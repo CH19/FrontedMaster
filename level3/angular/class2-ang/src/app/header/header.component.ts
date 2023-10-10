aad249ce705d2bf8b08d9077167b60702c8c8d3b
CH19 <chcc190505@gmail.com>
2023-10-10 10:33:05 -0400

    proyecto de la clase dos actualizado para agregar los ui components y ademas de eso se migraron los estilos a scss para utilizar los modulos necesarios

---
+++ level3/angular/class2-ang/src/app/header/header.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import {NgbConfig} from '@ng-bootstrap/ng-bootstrap';
+
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss']
+})
+export class HeaderComponent {
+  constructor(ngbConfig: NgbConfig) {
+    ngbConfig.animation = false;
+  }
+  colocar(){
+    const body =     document.querySelector('body');
+    if(body)     body.style.background = 'orange';
+  }
+}
